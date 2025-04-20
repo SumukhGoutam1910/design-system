c519ca0e337d1f6b7003e1b0b923152057468abc
Sumukh <sumukhgoutam0804@gmail.com>
2025-04-20 23:16:32 +0530

    20.04.2024 11:16pm

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,22 +1,43 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|ts|tsx|mjs)',
   ],
-  "addons": [
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions"
+  addons: [
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
   ],
-  "framework": {
-    "name": "@storybook/react-webpack5",
-    "options": {}
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
   },
-  "staticDirs": [
-    "..\\public"
-  ]
+  staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    config?.module?.rules?.push({
+      test: /\.css$/,
+      include: [path.resolve(__dirname, '..')],
+      use: [
+        'style-loader',
+        {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 1,
+          },
+        },
+        'postcss-loader',
+      ],
+    });
+  
+    return config;
+  },
 };
+
 export default config;
